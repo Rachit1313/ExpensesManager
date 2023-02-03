89b2e9a0e8bc6288d9bc27c92b4ba670bd6e0294
Rachit1313 <Rachitchawla33@gmail.com>
2023-02-03 02:45:53 -0500

    Organized UI/Expenses folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from './components/Expenses.js';
+import Expenses from './components/Expenses/Expenses.js';
 
 function App() {
   const expenses = [
@@ -25,7 +25,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      {/* <h2>Let's get started!</h2> */}
       <Expenses items={expenses} />
     </div>
   );
